1d92df0f3888b684693e8a87c718a5af860015f4
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-05-15 11:33:15 -0400

    Ejercicio guiado terminado

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,32 @@
    function agregarPosteo(){
-        var contenedor = documentElementById(contenedor-posteos)
+    var contenedor = document.getElementById("contenedor-posteos");
+    var nombreFF = document.getElementById("nombre").value;
+    var contenidoFF = document.getElementById("cajaposteos").value;
+    var nuevoPost = document.createElement("div");
+    var contenedorNombre = document.createElement("strong");
+    var contenedorPost = document.createElement("p");
+    var separacion = document.createElement("hr");
+    var parrafCorazon = document.createElement("p");
+    var i = document.createElement("i");
+    parrafCorazon.appendChild(i);
+    parrafCorazon.setAttribute("class", "corazon");
+    i.setAttribute("class", "fa-fa-heart");
+    i.setAttribute("arial-hidden", "true");
+    var nodoNombre = document.createTextNode(nombreFF + "escribio: ")
+    var nodoPosteo = document.createTextNode(contenidoFF);
+    contenedorNombre.appendChild(nodoNombre);
+    contenedorPost.appendChild(nodoPosteo);
+    nuevoPost.appendChild(contenedorNombre);
+    nuevoPost.appendChild(contenedorPost);
+    nuevoPost.appendChild(parrafCorazon);
+    nuevoPost.appendChild(separacion);
+    i.addEventListener("click", function(){
+        i.classList.toggle("rojo");
+    });
+    nuevoPost.setAttribute("class", "posteo");
+    contenedorPost.appendChild(nuevoPost);
+    document.getElementById("nombre").value = "";
+    document.getElementById("cajaposteos").value = "";
     var elem_p = document.createElement('p');
     var elem_em = document.createElement('em');
     var elem_strong = document.createElement('strong');
